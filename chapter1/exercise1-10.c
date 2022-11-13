a2f051d6eae4c3c7ae407dfdbf64b2f5d8d92991
Daniel R. Noonan <54192635+polyglotprog@users.noreply.github.com>
2022-11-13 16:41:10 -0600

    Exercise 1-10

---
+++ chapter1/exercise1-10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* copy input to output, replacing the following:
+      tab        with  \t
+      backspace  with  \b
+      backslash  with  \\ */
+main()
+{
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    if (c == '\t') {
+      putchar('\\');
+      putchar('t');
+    } else if (c == '\b') {
+      putchar('\\');
+      putchar('b');
+    } else if (c == '\\') {
+      putchar('\\');
+      putchar('\\');
+    } else
+      putchar(c);
+  }
+}
